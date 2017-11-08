627117f6b2e83c6a8d98ec42b9d7390da18f2eca
Michael Sutherland <michael.sutherland@dmea.osd.mil>
2017-11-07 16:42:20 -0800

    Added setup.py file to build and install package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='mpiarray',
+      version='1.0',
+      description='Numpy Arrays Distributed over MPI',
+      author='Michael Sutherland',
+      author_email='michael.sutherland@dmea.osd.mil',
+      py_modules=['mpiarray'],
+      )
